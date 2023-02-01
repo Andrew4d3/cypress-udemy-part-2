d189664e38a18ca7ed5be850cb2c9359d04cd345
Andrew Davis <andrew4d3@gmail.com>
2023-02-01 08:23:38 -0300

    Lecture 36 done

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -6,6 +6,25 @@ describe("Test with backend", () => {
   });
 
   it("should log in", () => {
-    cy.log("Hello");
+    cy.intercept("POST", "https://api.realworld.io/api/articles/").as(
+      "postArticles"
+    );
+
+    const now = Date.now();
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("This is the title " + now);
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the article");
+    cy.contains("Publish Article").click();
+
+    cy.wait("@postArticles").then((xhr) => {
+      expect(xhr.response.statusCode).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the article"
+      );
+      expect(xhr.response.body.article.description).to.equal(
+        "This is a description"
+      );
+    });
   });
 });
