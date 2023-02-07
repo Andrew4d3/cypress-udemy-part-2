1a85996b44e518205e66395db30bd35d1272286e
Andrew Davis <andrew4d3@gmail.com>
2023-02-07 08:42:55 -0300

    Lecture 40: Cy.Intercept() in details

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -2,9 +2,12 @@
 
 describe("Test with backend", () => {
   beforeEach(() => {
-    cy.intercept("GET", "https://api.realworld.io/api/tags", {
-      fixture: "tags.json",
-    });
+    cy.intercept(
+      { method: "Get", path: "tags" },
+      {
+        fixture: "tags.json",
+      }
+    );
     cy.loginToApplication();
   });
 
@@ -38,7 +41,36 @@ describe("Test with backend", () => {
       .and("contain", "testing");
   });
 
-  it.only("verify global feed likes count", () => {
+  it.only("intercepting and modifying the request and response", () => {
+    cy.intercept("POST", "https://api.realworld.io/api/articles/", (req) => {
+      req.body.article.description = "This is a description 2";
+      req.reply((res) => {
+        expect(res.body.article.description).to.equal(
+          "This is a description 2"
+        );
+        res.body.article.description = "This is a description 3";
+      });
+    }).as("postArticles");
+
+    const now = Date.now();
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("This is the title " + now);
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the article");
+    cy.contains("Publish Article").click();
+
+    cy.wait("@postArticles").then((xhr) => {
+      expect(xhr.response.statusCode).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the article"
+      );
+      expect(xhr.response.body.article.description).to.equal(
+        "This is a description 3"
+      );
+    });
+  });
+
+  it("verify global feed likes count", () => {
     cy.intercept("GET", "https://api.realworld.io/api/articles/feed*", {
       articles: [],
       articlesCount: 0,
