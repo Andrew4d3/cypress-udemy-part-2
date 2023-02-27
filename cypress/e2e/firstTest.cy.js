012596cf73d7cdab1aaca2898bc8152c0dcb93a5
Andrew Davis <andrew4d3@gmail.com>
2023-02-27 07:51:40 -0300

    Lecture 49

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -28,6 +28,52 @@ describe("Test with backend", () => {
     });
   });
 
+  it("should log in firefox", { browser: "firefox" }, () => {
+    cy.intercept("POST", "https://api.realworld.io/api/articles/").as(
+      "postArticles"
+    );
+
+    const now = Date.now();
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("This is the title " + now);
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the article");
+    cy.contains("Publish Article").click();
+
+    cy.wait("@postArticles").then((xhr) => {
+      expect(xhr.response.statusCode).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the article"
+      );
+      expect(xhr.response.body.article.description).to.equal(
+        "This is a description"
+      );
+    });
+  });
+
+  it("should not log in firefox", { browser: ["!firefox", "!edge"] }, () => {
+    cy.intercept("POST", "https://api.realworld.io/api/articles/").as(
+      "postArticles"
+    );
+
+    const now = Date.now();
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("This is the title " + now);
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the article");
+    cy.contains("Publish Article").click();
+
+    cy.wait("@postArticles").then((xhr) => {
+      expect(xhr.response.statusCode).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the article"
+      );
+      expect(xhr.response.body.article.description).to.equal(
+        "This is a description"
+      );
+    });
+  });
+
   // This will overide global config
   it("verify popular tags are displayed", { retries: 2 }, () => {
     cy.intercept(
